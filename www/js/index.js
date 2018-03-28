d15be50c51145a816d6c0485e7d7069596411a06
mithun1 <webappadv@gmail.com>
2018-03-28 04:27:15 -0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -49,11 +49,11 @@ var app = {
         var isAndroid = (/(android)/i.test(navigator.userAgent));
         var adPublisherIds = {
             ios: {
-                banner: 'ca-app-pub-3940256099942544/6300978111',
+                banner: 'ca-app-pub-1458752546991864/3679243103',
                 interstitial: '	ca-app-pub-3940256099942544/1033173712'
             },
             android: {
-                banner: 'ca-app-pub-3940256099942544/6300978111',
+                banner: 'ca-app-pub-1458752546991864/3679243103',
                 interstitial: 'ca-app-pub-3940256099942544/1033173712'
             }
         };
